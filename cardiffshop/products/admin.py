9171afcf93f9409491dad9ddb45ab3c84994571b
nature613 <chikonakamura613@gmail.com>
2015-06-04 00:07:43 +0100

    implemented can be sold list filter

--- cardiffshop/products/admin.py
+++ cardiffshop/products/admin.py
@@ -1,10 +1,29 @@
 from products.models import Product, Category, ProductImage
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
+
+
+class CanBeSoldListFilter(SimpleListFilter):
+    title = "Can be sold"
+    parameter_name = "can_be_sold"
+
+    def lookups(self, request, model_admin):
+        return (
+            ("1", "Yes"),
+            ("0", "No")
+        )
+
+    def queryset(self, request, queryset):
+        value = self.value()
+        if value == "1":
+            return queryset.filter(stock_count__gt=0)
+        if value == "0":
+            return queryset.filter(stock_count=0)
 
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "stock_count", "can_be_sold")
-    list_filter = ("category", "date_created")
+    list_filter = ("category", "date_created", CanBeSoldListFilter)
     search_fields = ("name", "description", "sku_number", "barcode")
     fieldsets = (
         ("Identity", {
