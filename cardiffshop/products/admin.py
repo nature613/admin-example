ea15c33c5c17eea01ff643044143ca20daf60890
nature613 <chikonakamura613@gmail.com>
2015-06-03 23:17:16 +0100

    Improve admin

--- cardiffshop/products/admin.py
+++ cardiffshop/products/admin.py
@@ -1,6 +1,12 @@
-from products.models import Category
-from products.models import Product
 from django.contrib import admin
+from products.models import Category, Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category")
+    list_filter = ("category", "date_created")
+    search_fields = ("name", "description", "sku_number", "barcode_number")
+
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
