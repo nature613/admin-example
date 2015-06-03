f304fc078131632cf72d464cb1e96589770c1317
nature613 <chikonakamura613@gmail.com>
2015-06-03 22:17:31 +0100

    Added new fields

--- cardiffshop/products/models.py
+++ cardiffshop/products/models.py
@@ -20,9 +20,14 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField(verbose_name=_("Name"), max_length=255)
+    description = models.TextField(_("Description"))
     category = models.ForeignKey(Category, verbose_name=_("Category"), related_name="products")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     price_unit = models.CharField(_("Price Unit"), choices=PRICE_UNIT_CHOICES, max_length=10)
+
+    sku_number = models.CharField(_("SKU number"), blank=True, null=False, max_length=255)
+    barcode_number = models.CharField(_("SKU number"), max_length=255)
+
     is_visible = models.BooleanField(verbose_name=_("Visible"), default=True)
     date_created = models.DateTimeField(_("Date Created"), auto_now_add=True)
 
