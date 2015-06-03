4e6e3eac15bf523ee25202ca80f421dd8b9767ad
nature613 <chikonakamura613@gmail.com>
2015-06-03 23:52:08 +0100

    Add new admin properties

--- cardiffshop/products/admin.py
+++ cardiffshop/products/admin.py
@@ -3,9 +3,31 @@ from django.contrib import admin
 
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("name", "category", "stock_count")
+    list_display = ("name", "category", "stock_count", "can_be_sold")
     list_filter = ("category", "date_created")
     search_fields = ("name", "description", "sku_number", "barcode")
+    fieldsets = (
+        ("Identity", {
+            "fields": ("name", "slug"),
+        }),
+        ("rr", {
+            "fields": ("description", "price", "price_unit"),
+        }),
+        ("ww", {
+            "fields": ("category", "barcode", "sku_number", "stock_count", "is_visible"),
+        }),
+    )
+
+    def can_be_sold(self, obj):
+        """
+        Determines wheter the product can be sold or not.
+        """
+        if obj.stock_count > 0:
+            return True
+        else:
+            return False
+
+    can_be_sold.boolean = True
 
 
 admin.site.register(Category)
