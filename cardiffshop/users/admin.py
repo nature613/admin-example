eb322cf346efbb2feec5ff72eee68dca26464e54
nature613 <chikonakamura613@gmail.com>
2015-06-03 22:50:47 +0100

    Improve Admin

--- cardiffshop/users/admin.py
+++ cardiffshop/users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from users.models import User
 
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
